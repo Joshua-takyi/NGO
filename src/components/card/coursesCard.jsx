aef91ac99ff0a7e3bc1ff0bda2c320f7e5c3afff
joshua Takyi <takyijoshua019@gmail.com>
2024-06-20 21:56:52 +0000

    fixed 5

--- src/components/card/coursesCard.jsx
+++ src/components/card/coursesCard.jsx
@@ -11,26 +11,26 @@ import Link from "next/link";
 
 export function CoursesCard({ header, description, image }) {
 	return (
-		<Card className="lg:w-[330px] lg:h-[500px] md:h-[460px]  md:w-[350px] w-[300px]">
-			<CardHeader>
-				<CardTitle>
+		<Card className="lg:w-[330px] lg:h-[500px] md:h-[460px] md:w-[350px] w-[300px] h-auto overflow-hidden">
+			<CardHeader className="overflow-hidden">
+				<CardTitle className="h-full">
 					<Image
 						src={image}
 						alt="image"
+						layout="responsive"
 						width={200}
 						height={300}
-						className="w-full  rounded-md"
+						className="w-full h-auto rounded-md"
 					/>
 				</CardTitle>
-				<CardDescription className="p-2"></CardDescription>
 			</CardHeader>
-			<CardContent className="p-4 flex flex-col text-md text-muted-foreground gap-4 text-center justify-center">
+			<CardContent className="p-4 flex flex-col text-md text-muted-foreground gap-4 text-center justify-center overflow-hidden">
 				<h5 className="font-bold text-black lg:text-[23px]">{header}</h5>
 				<p>{description}</p>
 			</CardContent>
 			<CardFooter className="p-5 flex items-end h-[10%]">
 				<Link
-					href={"/"}
+					href="/"
 					className="hover:scale-[1.1] hover:text-[#FFC100] transition-all"
 				>
 					Read More <span>→</span>
